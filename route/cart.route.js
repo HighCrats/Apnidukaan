f9565c09806586fb76dedf8b42bdbc741d8a4727
ajju <coderajju@gmail.com>
2023-04-19 16:50:37 +0530

    Cart Added

---
+++ route/cart.route.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { addToCart,cartData} from "../controller/cart.controller.js";
+const router =express.Router();
+
+router.post("/add-to-cart",addToCart);
+router.get("/:userId",cartData);
+export default router;
