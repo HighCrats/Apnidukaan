cb5e613a6c6bdfa9bb040a79796b9d0241c90cce
lokeshingle2020@gmail.com <lokesh>
2023-04-19 17:03:49 +0530

    Category And User Added

---
+++ controller/category.controller.js
@@ -0,0 +1,8 @@
+export const list = (request,response)=>{
+    Category.find()
+    .then(result=>{
+        return response.status(200).json({categories: result, status: true});
+    }).catch(err=>{
+        return response.status(500).json({error: "Internal Server Error", status: false});
+    })
+};
---
+++ model/category.model.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+const categorySchema = new mongoose.Schema({
+    categoryName: String
+});
+
+export const Category = mongoose.model("category",categorySchema);
+
+
