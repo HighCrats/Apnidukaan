17148da4d0ef0cd162fe19027667bb443417d7cf
ajju <coderajju@gmail.com>
2023-05-08 14:45:42 +0530

    category with image

--- controller/sell.controller.js
+++ controller/sell.controller.js
@@ -3,20 +3,15 @@ import Seller from "../model/seller.model.js";
 
 export const sellProduct = async(request,response,next)=>{
     try{
-        const images = req.files.map(file => ({
-            data: file.buffer,
-            contentType: file.mimetype
-          }));
         const description = request.body.description;
         const title = request.body.title;
         const price = request.body.price;
-        const bill = request.file.filename;                
-        console.log(request.file);
+        const bill = request.file.filename;                                           
         request.body.bill = request.file.filename;
         const errors= await validationResult(request);
         if(!errors.isEmpty())
         return response.status(400).json({ error: "Bad request", messages: errors.array() });
-        let seller = await Seller.create({price,description,title,bill,images});
+        let seller = await Seller.create({price,description,title,bill});
             return response.status(200).json({ seller: seller, status: true });    
     }
     catch (err) {
--- route/sell.route.js
+++ route/sell.route.js
@@ -3,6 +3,5 @@ import {sellProduct} from "../controller/sell.controller.js";
 import multer from "multer";
 const router = express.Router();
 const upload = multer({dest:"public/billImages"});
-const storage = multer.memoryStorage();
-router.post("/sellproduct",upload.single("bill"),upload.array('images'),sellProduct);
+router.post("/sellproduct",upload.single("bill"),sellProduct);
 export default router;
