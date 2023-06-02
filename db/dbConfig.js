4f564f7b06b1ddf80cab534ed3256473b5d31205
HighCrats <131246898+HighCrats@users.noreply.github.com>
2023-06-02 14:34:01 +0530

    Update dbConfig.js

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://harshitasisodiya:harshita2810@cluster0.wytrnkb.mongodb.net/apnidukaan?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://user1:user123@cluster0.wytrnkb.mongodb.net/apnidukaan?retryWrites=true&w=majority")
     .then(result => {
         console.log("Database Connected");
     })
