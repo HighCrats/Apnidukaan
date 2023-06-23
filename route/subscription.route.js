2d8c3b10170ea34f988c17fbe95fea2007acdf77
chiragvakare <chiragvakare2904@gmail.com>
2023-06-23 14:57:02 +0530

    backend complete

--- route/subscription.route.js
+++ route/subscription.route.js
@@ -1,13 +1,9 @@
 import express from "express";
-import {  expireSubscription, sendOtp, showSubscriptions, subscription, updateBalance } from "../controller/subscription.controller.js";
+import { addSubscription } from "../controller/subscription.controller.js";
 
 const router = express.Router();
 
-
-router.post("/Subscription", subscription)
-router.post('/expireSubscription',expireSubscription)
-router.post('/showSubscription',showSubscriptions)
-router.post('/sendotp',sendOtp);
-router.post('updateBalance',updateBalance)
+//add subscription
+router.post("/addSubscription", addSubscription);
 
 export default router;
